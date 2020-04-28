df4187d21b0032044b6c033a61d3e3f78cff6919
Saurav Arya <aryasaurav25@gmail.com>
2020-04-28 17:14:57 +0530

    Added questions & answers

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -34,9 +34,76 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
       ),
-      body: FutureBuilder(
-        future: fetchQuestions(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {},
+      body: RefreshIndicator(
+        onRefresh: fetchQuestions,
+        child: FutureBuilder(
+          future: fetchQuestions(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Container(
+                child: getQuestionsList(),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  ListView getQuestionsList() {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: ExpansionTile(
+            title: ListTile(
+              title: Text(
+                results[index].question,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(results[index].category),
+            ),
+            children: results[index].allAnswers.map((option) {
+              return AnswerWidget(results, index, option);
+            }).toList(),
+          ),
+        );
+      },
+    ); //ListView.build
+  }
+} //State class end
+
+class AnswerWidget extends StatefulWidget {
+  final List<Results> results;
+  final int index;
+  final String option;
+  AnswerWidget(this.results, this.index, this.option);
+  @override
+  _AnswerWidgetState createState() => _AnswerWidgetState();
+}
+
+class _AnswerWidgetState extends State<AnswerWidget> {
+  Color tick;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        setState(() {
+          if (widget.option == widget.results[widget.index].correctAnswer) {
+            tick = Colors.green;
+          } else {
+            tick = Colors.red;
+          }
+        });
+      },
+      title: Text(
+        widget.option,
+        textAlign: TextAlign.center,
+        style: TextStyle(color: tick),
       ),
     );
   }
